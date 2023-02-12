cd1ffc692ada2328968dca719e711c684ecfd026
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-02-12 23:22:17 +0900

    feat : add username and make login api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,32 @@
 const express = require("express")
 const app = express()
+app.use(express.json())
 
-app.listen(5000, () => console.log("Backend server is running!"))
+const users = [
+  {
+    id: "1",
+    username: "john",
+    password: "john123",
+    isAdmin: true,
+  },
+  {
+    id: "2",
+    username: "jane",
+    password: "jane123",
+    isAdmin: false,
+  },
+]
+
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body
+  const user = users.find(
+    u => u.username === username && u.password === password
+  )
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(400).json("Wrong username or password!")
+  }
+})
+
+app.listen(9000, () => console.log("Backend server is running!"))
